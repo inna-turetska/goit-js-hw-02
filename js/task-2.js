5ba144483f06c7529fd7453f5b110cf7f5c46292
Inna Turetska <innaturetska1987@gmail.com>
2024-11-06 14:52:05 +0200

    task java 2 is done

--- js/task-2.js
+++ js/task-2.js
@@ -1,9 +1,13 @@
-
- function getShippingMessage(country, price, deliveryFree) {
-    const totalPrice = price + deliveryFree;
-    return `Shipping to ${country} will cost ${totalPrice} credits!`;
+function formatMessage(message, maxLength) {
+    return message.length > maxLength
+        ? message.substring(0, maxLength) + "..."
+        : message;
 }
 
-console.log(getShippingMessage("Australia", 120, 50)); 
-console.log(getShippingMessage("Germany", 80, 20)); 
-console.log(getShippingMessage("Sweden", 100, 20)); 
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
+
--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,11 @@
- function getElementWidth(content,padding,border) {
-    content = Number.parseFloat(content);
-    padding = Number.parseFloat(padding)*2;
-    border = Number.parseFloat(border)*2;
-    const getElementWidth = content + padding + border;
-    return getElementWidth;
+function checkForSpam(message) {
+    return message.toLowerCase().includes("sale".toLowerCase())|| message.toLowerCase().includes("spam".toLowerCase() )
+   
 }
-
-console.log(getElementWidth("50px", "8px", "4px")); 
-console.log(getElementWidth("60px", "12px", "8.5px")); 
-console.log(getElementWidth("200px", "0px", "0px")); 
+ console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
